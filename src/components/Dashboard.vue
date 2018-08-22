54db61830798a915985508727a46e1556da5da9b
Graham Olson <olsongraham32@gmail.com>
2018-08-21 20:43:33 -0600

    initial commit

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="hello">
+    <div class="row">
+      <div class="col-12">
+        <h1>Dashboard</h1>
+        <button type="button" class="btn btn-dark">Cars</button>
+        <button type="button" class="btn btn-dark">Houses</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Dashboard',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
